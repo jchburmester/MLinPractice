19935b40a9b6553a0cc23ba40030b1986f89a587
Jasper <jasperol@hotmail>
2021-10-09 14:40:16 +0200

    added Tokenization code
    worked through the tutorial creating the tokenization code

---
+++ code/preprocessing/tokenization.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tokenize the tweet into individual words
+"""
+
+from code.preprocessing.preprocessor import Preprocessor
+import nltk
+
+class Tokenizer(Preprocessor):
+    """ Tokenizes the input column into individual words """
+    
+    def _init_(self, input_column, output_column):
+        """Initialize the Tokenizer with the given in and output column"""
+        super()._init_([input_column], output_column)
+        
+    def _get_values(self, inputs):
+        "Tokenize the tweet"
+        
+        tokenized = []
+        
+        for tweet in inputs[0]:
+            sentences = nltk.sent_tokenize(tweet)
+            tokenized_tweet = []
+            for sentence in sentences:
+                words = nltk.word_tokenize(sentence)
+                tokenized_tweet += words
+                
+            tokenized.append(str(tokenized_tweet))
+            
+        return tokenized
